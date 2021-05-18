7ea8084076ce9dc83b8a01d239ec3d493d2be536
Rahmat ahmadi <rahmatullahahmadi500@gmail.com>
2021-05-18 10:55:10 +0200

    Add exercice 6

---
+++ 288 Antoine/Exercices/Exercice6/script.js
@@ -0,0 +1,23 @@
+/* function carre(x){
+    return x * x;
+}
+const carre2 = (carre) => carre * carre ;
+console.log(carre2(0));
+console.log(carre2(2));
+console.log(carre2(5));
+console.log(carre2(10)); */
+
+/* function min(nombre1, nombre2){
+    if(nombre1 > nombre2){
+        return nombre2;
+    }else if(nombre1 < nombre2){
+        return nombre1;
+    }else if (nombre1 === nombre2){
+        return nombre1;
+    }
+}
+console.log(min(4.5, 5));
+console.log(min(19, 9));
+console.log(min(5, 5));
+ */
+const min2 = (nombre1, nombre2) => {if(nombre1 > nombre2) return nombre2, {if}}
